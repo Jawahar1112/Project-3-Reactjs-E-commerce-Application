4dab91cb605aa44e31aaeab91f08ee75d7e4e274
Jawaharlal G <jawaharlal.g11@gmail.com>
2025-09-06 17:34:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,4 @@
 // Updated for testing
 // Updated for testing
 // Updated for testing
-<<<<<<< HEAD
-// Updated for Prod Pipelinf testing
-=======
-// Updated for testing
->>>>>>> dev
 // Updated for testing
